b51bb2440cc3114bfb1e95c192b89513c5e6b001
Lucas Pedreira <lucasvital@outlook.com>
2024-10-18 13:37:51 -0400

    datatable

---
+++ resources/js/Pages/Communication/DataTableDropDown.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { Button } from '@/shadcn/ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/shadcn/ui/dropdown-menu'
+import { MoreHorizontal } from 'lucide-vue-next'
+
+const props = defineProps({
+    payment: {
+        id: String
+    }
+})
+
+function copy(id) {
+    navigator.clipboard.writeText(id)
+}
+</script>
+
+<template>
+    <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+            <Button variant="ghost" class="w-8 h-8 p-0">
+                <span class="sr-only">Open menu</span>
+                <MoreHorizontal class="w-4 h-4" />
+            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem @click="copy(payment.id)">
+                Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+</template>
