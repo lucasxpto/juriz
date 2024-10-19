ff6a9a2c7a93a26fe2e7ac03bf8ce640439c3aa9
Lucas Pedreira <lucasvital@outlook.com>
2024-10-19 11:44:15 -0400

    wip

--- app/Services/Comunica/ComunicaService.php
+++ app/Services/Comunica/ComunicaService.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace App\Services\Comunica;
 
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
@@ -17,6 +18,9 @@ class ComunicaService
         $this->http = Http::baseUrl('https://comunicaapi.pje.jus.br/api/v1');
     }
 
+    /**
+     * @throws ConnectionException
+     */
     public function getCommunications($url): Response
     {
         return $this->http->get('/comunicacao?' . $url);
