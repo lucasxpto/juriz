c1afa261f87b54447e43aca1ef261cdfcd5da9b0
Lucas Pedreira <lucasvital@outlook.com>
2024-10-21 23:19:06 -0400

    wip

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
-use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
 
 class HorizonServiceProvider extends HorizonApplicationServiceProvider
@@ -31,7 +30,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user) {
             return in_array($user->email, [
-                //
+                'info@perta.io',
             ]);
         });
     }
